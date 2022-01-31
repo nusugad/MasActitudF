5ee7f86792586ce7f8abd186c3d70ad1e060f2c1
Nud <nusugad@outlook.com>
2022-01-30 21:09:12 -0500

    Inicio de construcción de controladores y vistas por areas

---
+++ MasActitud/Areas/Admin/Controllers/Trabajadores.cs
@@ -0,0 +1,41 @@
+﻿using MasActitud.Data;
+using MasActitud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasActitud.Areas.Admin.Controllers
+{
+    public class Trabajadores : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Trabajadores(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Trabajador.ToListAsync());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Trabajador trabajador)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Trabajador.Add(trabajador);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+    }
+}
